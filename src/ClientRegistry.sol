108d409118dcdcd7333a96cadf05b4203622065a
desbenn <desbenn@gmail.com>
2025-11-06 12:35:15 -0500

    Updates

--- src/ClientRegistry.sol
+++ src/ClientRegistry.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import "lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
+//import "lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/AccessControl.sol";
 
 // cast call 0x5FbDB2315678afecb367f032d93F642f64180aa3 "isClientActive(address)" 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 --rpc-url http://127.0.0.1:8545
 
